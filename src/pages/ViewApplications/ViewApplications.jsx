0a8711e8d4cdbf5d2de0e271f771c6d57652b045
rummansaaqeb <rsaaqeb@gmail.com>
2024-12-24 16:40:31 +0600

    updated job application status

--- src/pages/ViewApplications/ViewApplications.jsx
+++ src/pages/ViewApplications/ViewApplications.jsx
@@ -3,6 +3,26 @@ import { useLoaderData } from 'react-router-dom';
 
 const ViewApplications = () => {
     const applications = useLoaderData();
+
+
+    const handleStatusUpdate = (e, id) => {
+        console.log(e.target.value, id);
+        const data = {
+            status: e.target.value,
+        }
+        fetch(`http://localhost:5000/job-applications/${id}`, {
+            method: "PATCH",
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        })
+    }
+
     return (
         <div>
             <h2 className="text-3xl">Applications For This Job: {applications.length}</h2>
@@ -13,8 +33,8 @@ const ViewApplications = () => {
                         <tr>
                             <th></th>
                             <th>Email</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
+                            <th>Status</th>
+                            <th>Update Status</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -23,7 +43,15 @@ const ViewApplications = () => {
                                 <th>{index + 1}</th>
                                 <td>{app.applicant_email}</td>
                                 <td>Quality Control Specialist</td>
-                                <td>Blue</td>
+                                <td>
+                                    <select onChange={(e) => handleStatusUpdate(e, app._id)} defaultValue={app.status || 'Change Status'} className="select select-bordered select-xs w-full max-w-xs">
+                                        <option disabled>Change Status</option>
+                                        <option>Under Review</option>
+                                        <option>Set Interview</option>
+                                        <option>Hired</option>
+                                        <option>Rejected</option>
+                                    </select>
+                                </td>
                             </tr>)
                         }
                     </tbody>
