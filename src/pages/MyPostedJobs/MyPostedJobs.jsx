9dd0c3afc0b15a4e1882323bc7a5537e0a3d2b6c
rummansaaqeb <rsaaqeb@gmail.com>
2024-12-24 16:09:38 +0600

    showed my posted jobs and counted job applications

--- src/pages/MyPostedJobs/MyPostedJobs.jsx
+++ src/pages/MyPostedJobs/MyPostedJobs.jsx
@@ -7,15 +7,37 @@ const MyPostedJobs = () => {
     const { user } = useAuth();
     useEffect(() => {
         fetch(`http://localhost:5000/jobs?email=${user.email}`)
-        .then(res => res.json())
-        .then(data => {
-            setJobs(data);
-        })
+            .then(res => res.json())
+            .then(data => {
+                setJobs(data);
+            })
     }, [user.email])
 
     return (
-        <div>
-            <h2 className='text-3xl'>My Posted Jobs : {jobs.length}</h2>
+        <div className="overflow-x-auto">
+            <table className="table">
+                {/* head */}
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Job Title</th>
+                        <th>Deadline</th>
+                        <th>Application Count</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        jobs.map((job, index) =>
+                            <tr key={index}>
+                                <th>{index + 1}</th>
+                                <td>{job.title}</td>
+                                <td>{job.applicationDeadline}</td>
+                                <td>{job.applicationCount}</td>
+                            </tr>
+                        )
+                    }
+                </tbody>
+            </table>
         </div>
     );
 };
