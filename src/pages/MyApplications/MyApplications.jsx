2652c74b7dee74d2b75c6af2da5bd0fb7d3e4fb3
rummansaaqeb <rsaaqeb@gmail.com>
2024-12-26 22:21:41 +0600

    created custom hook for get request at my applications component

--- src/pages/MyApplications/MyApplications.jsx
+++ src/pages/MyApplications/MyApplications.jsx
@@ -1,14 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import useAuth from '../../hooks/useAuth';
+import axios from 'axios';
+import useAxiosSecure from '../../hooks/useAxiosSecure';
 
 const MyApplications = () => {
     const { user } = useAuth();
     const [jobs, setJobs] = useState([]);
+    const axiosSecure = useAxiosSecure();
 
     useEffect(() => {
-        fetch(`http://localhost:5000/job-application?email=${user.email}`)
-            .then(res => res.json())
-            .then(data => setJobs(data))
+        // fetch(`http://localhost:5000/job-application?email=${user.email}`)
+        //     .then(res => res.json())
+        //     .then(data => setJobs(data))
+
+        // axios.get(`http://localhost:5000/job-application?email=${user.email}`, { withCredentials: true })
+        //     .then(res => setJobs(res.data));
+
+        axiosSecure.get(`job-application?email=${user.email}`)
+        .then(res => setJobs(res.data));
+
+        
+
     }, [user.email])
 
     return (
