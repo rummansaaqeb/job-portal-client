4757c6d35f6bdc1c96822d9bc95ba3f937a63103
rummansaaqeb <rsaaqeb@gmail.com>
2025-01-09 12:21:31 +0600

    added search functionality in addJobs page

--- src/hooks/useJobs.jsx
+++ src/hooks/useJobs.jsx
@@ -1,19 +1,19 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
-const useJobs = (sort) => {
+const useJobs = (sort, search) => {
     const [jobs, setJobs] = useState([]);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        axios.get(`http://localhost:5000/jobs?sort=${sort}`)
-        .then(res => {
-            setLoading(false);
-            setJobs(res.data);
-        })
-    }, [sort])
+        axios.get(`http://localhost:5000/jobs?sort=${sort}&search=${search}`)
+            .then(res => {
+                setLoading(false);
+                setJobs(res.data);
+            })
+    }, [sort, search])
 
-    return {jobs, loading}
+    return { jobs, loading }
 };
 
 export default useJobs;
--- src/pages/AllJob/AllJob.jsx
+++ src/pages/AllJob/AllJob.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import useJobs from '../../hooks/useJobs';
 import HotJobCard from '../Home/HotJobCard';
+import { BiSearch } from 'react-icons/bi';
 
 const AllJob = () => {
     const [sort, setSort] = useState(false);
-    const { jobs, loading } = useJobs(sort);
+    const [search, setSearch] = useState("");
+    const { jobs, loading } = useJobs(sort, search);
     console.log(sort);
     if (loading) {
         return <span className='loading loading-spinner loading-lg'></span>
@@ -12,8 +14,10 @@ const AllJob = () => {
     return (
         <div>
             <h1 className='py-5 text-4xl text-center font-bold'>All Jobs</h1>
-            <div className='w-11/12 mx-auto bg-base-200 py-5 p-3 flex items-center'>
+            <div className='w-11/12 mx-auto bg-base-200 py-5 p-3 flex items-center gap-5'>
                 <button onClick={() => setSort(!sort)} className={`btn btn-neutral ${sort && 'btn-success'}`}>{sort === true ? 'Sorted By Salary' : 'Sort By Salary'}</button>
+                <BiSearch className='w-7 h-7'></BiSearch>
+                <input onKeyUp={(e) => setSearch(e.target.value)} type="text" className='input w-full max-w-2xl' name="" placeholder='Search Jobs By Location' />
             </div>
             <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
                 {
